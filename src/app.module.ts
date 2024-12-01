841846731f33f45a48aef38cda336bba3d2f5d31
unknown <aasthakhanal02@gmail.com>
2024-12-01 15:58:00 +0545

    completed organizations CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RolesModule } from './roles/roles.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { OrganizationsModule } from './organizations/organizations.module';
 
 @Module({
-  imports: [RolesModule, PrismaModule],
+  imports: [RolesModule, PrismaModule, OrganizationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/organizations/organizations.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { OrganizationsService } from './organizations.service';
+import { OrganizationsController } from './organizations.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+
+@Module({
+  controllers: [OrganizationsController],
+  providers: [OrganizationsService,PrismaService],
+})
+export class OrganizationsModule {}
+
+
